75838edbcffb6880c89eda87752c80aac9c2400f
john van strien <john2525@earthlink.net>
2015-07-10 11:23:53 -0700

    Added a member and some methods to enum.

--- app/src/main/java/com/biotronisis/pettplant/communication/ConnectionState.java
+++ app/src/main/java/com/biotronisis/pettplant/communication/ConnectionState.java
@@ -2,9 +2,24 @@ package com.biotronisis.pettplant.communication;
 
 public enum ConnectionState {
 	
-	NONE,          // we're doing nothing
-	FAILED,        // an attempted connection failed
-	LISTENING,     // now listening for incoming connections
-	CONNECTING,    // now initiating an outgoing connection
-	ESTABLISHED;   // now connected to a remote device
+	NONE(0),          // we're doing nothing
+	FAILED(1),        // an attempted connection failed
+	LISTENING(2),     // now listening for incoming connections
+	CONNECTING(3),    // now initiating an outgoing connection
+	ESTABLISHED(4);   // now connected to a remote device
+
+	private int id;
+
+	private ConnectionState(int id) {
+		this.id = id;
+	}
+
+	public int getValue() {
+		return id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
 }
