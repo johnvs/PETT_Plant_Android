e256fbcdbd6126534a1d0811da6e014032ff3892
john van strien <john2525@earthlink.net>
2015-07-29 14:24:23 -0700

    Adapting class to this project.

--- app/src/main/java/com/biotronisis/pettplant/communication/Transfer/RunEntrainmentCommand.java
+++ app/src/main/java/com/biotronisis/pettplant/communication/Transfer/RunEntrainmentCommand.java
@@ -1,23 +1,22 @@
 package com.biotronisis.pettplant.communication.transfer;
 
+import com.biotronisis.pettplant.type.EntrainmentMode;
+
 public class RunEntrainmentCommand extends AbstractCommand<EmptyResponse> {
 	private static final long serialVersionUID = 1L;
 	
 	public static final Byte COMMAND_ID = (byte)0x02;
 	
-	private int dutyCycle;
+	private EntrainmentMode eMode;
 	
 	@Override
 	public byte[] toCommandBytes() {
-		byte msb = asMsb(dutyCycle);
-		byte lsb = asLsb(dutyCycle);
-		
-		byte[] bytes = new byte[5];
+
+		byte[] bytes = new byte[3];
 		bytes[0] = 4;
 		bytes[1] = COMMAND_ID;
-		bytes[2] = msb;
-		bytes[3] = lsb;
-		bytes[4] = computeChecksum(bytes);
+		bytes[2] = (byte)eMode.getValue();
+		bytes[3] = computeChecksum(bytes);
 		
 		return bytes;
 	}
@@ -37,11 +36,11 @@ public class RunEntrainmentCommand extends AbstractCommand<EmptyResponse> {
 		return EmptyResponse.RESPONSE_ID;
 	}
 
-	public int getDutyCycle() {
-		return dutyCycle;
-	}
+//	public int getDutyCycle() {
+//		return dutyCycle;
+//	}
 
-	public void setDutyCycle(int dutyCycle) {
-		this.dutyCycle = dutyCycle;
+	public void setEntrainmentSequence(EntrainmentMode eMode) {
+		this.eMode = eMode;
 	}
 }
