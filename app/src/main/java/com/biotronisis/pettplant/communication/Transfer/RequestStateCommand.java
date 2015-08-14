bf5238ac9b0593215720bb485f1efe1c072e8ad7
john van strien <john2525@earthlink.net>
2015-08-14 16:53:45 -0700

    Added a new command to request the plant's current state.

---
+++ app/src/main/java/com/biotronisis/pettplant/communication/Transfer/RequestStateCommand.java
@@ -0,0 +1,43 @@
+package com.biotronisis.pettplant.communication.transfer;
+
+import com.biotronisis.pettplant.type.EntrainmentMode;
+
+public class RequestStateCommand extends AbstractCommand<RunEntrainmentResponse> {
+	private static final long serialVersionUID = 1L;
+	
+	public static final Byte COMMAND_ID = (byte)0x10;
+	
+	private EntrainmentMode eMode;
+	
+	@Override
+	public byte[] toCommandBytes() {
+
+		byte[] bytes = new byte[4];
+		bytes[0] = 3;                        // Number of message bytes to follow
+		bytes[1] = COMMAND_ID;
+		bytes[2] = (byte)(eMode.getValue() + 0x01); // Add 0x01 to translate the sequence numbers into command data format
+		                                            // See the app's readme.md file for plant command structure
+		bytes[3] = computeChecksum(bytes);
+		
+		return bytes;
+	}
+
+	@Override
+	public Byte getCommandId() {
+		return COMMAND_ID;
+	}
+
+	@Override
+	public Class<RunEntrainmentResponse> getResponseClass() {
+		return RunEntrainmentResponse.class;
+	}
+	
+	@Override
+	public Byte getResponseId() {
+		return RunEntrainmentResponse.RESPONSE_ID;
+	}
+
+	public void setEntrainmentSequence(EntrainmentMode eMode) {
+		this.eMode = eMode;
+	}
+}
