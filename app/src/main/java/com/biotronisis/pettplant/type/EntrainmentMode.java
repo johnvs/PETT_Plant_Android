9edcb109df1b8b08cfc0b24b65a434687f4562c5
john van strien <john2525@earthlink.net>
2015-08-14 17:05:57 -0700

    Commented out code in anticipation of deleting it.

--- app/src/main/java/com/biotronisis/pettplant/type/EntrainmentMode.java
+++ app/src/main/java/com/biotronisis/pettplant/type/EntrainmentMode.java
@@ -5,19 +5,23 @@ package com.biotronisis.pettplant.type;
  */
 public enum EntrainmentMode {
 
-   MEDITATE(0),
-   SLEEP(1),
-   STAY_AWAKE(2);
+   oldMEDITATE(0),
+   oldSLEEP(1),
+   oldSTAY_AWAKE(2);
 
    // Run/Stop button values
-   public static final String RUN = "Run";
-   public static final String STOP = "Stop";
+//   public static final String RUN = "Run";
+//   public static final String STOP = "Stop";
 
    // Pause/Resume button values
-   public static final String PAUSE = "Pause";
-   public static final String RESUME = "Resume";
+//   public static final String PAUSE = "Pause";
+//   public static final String RESUME = "Resume";
 
-   public static final boolean LOOP_CHECKBOX_DEFAULT = false;
+   // Loop checkbox values
+//   public static final String LOOP_OFF = "Off";
+//   public static final String LOOP_ON = "On";
+//
+//   public static final boolean LOOP_CHECKBOX_DEFAULT = false;
 
    private int id;
 
@@ -25,15 +29,15 @@ public enum EntrainmentMode {
       this.id = id;
    }
 
-   public int getValue() {
-      return id;
-   }
-
-   public int getId() {
-      return id;
-   }
-
-   public static EntrainmentMode getEntrainmentMode(int id) {
-      return EntrainmentMode.values()[id];
-   }
+//   public int getValue() {
+//      return id;
+//   }
+//
+//   public int getId() {
+//      return id;
+//   }
+//
+//   public static EntrainmentMode getEntrainmentMode(int id) {
+//      return EntrainmentMode.values()[id];
+//   }
 }
